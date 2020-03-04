2fd777003489ab76a442af0981ef0f24afbfcea6
Leftddr <qhddnjs75@gmail.com>
2020-03-03 19:42:11 -0800

    example

--- main_app.js
+++ main_app.js
@@ -1,10 +1,12 @@
 var http = require('http');
 
-var server = http.createServer()
+var server = http.createServer();
 
-var host = '192.168.0.46'
+var host = '192.168.0.46';
 var port = 8001;
 
 server.listen(port, host, '50000', function(){
     console.log("web_server started");
 });
+
+console.log('hhi');

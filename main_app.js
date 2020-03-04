69fcbf62052c00e4d0016236dea0c21114ab2234
Leftddr <qhddnjs75@gmail.com>
2020-03-04 13:15:19 +0900

    change

--- main_app.js
+++ main_app.js
@@ -3,10 +3,9 @@ var http = require('http');
 var server = http.createServer();
 
 var host = '192.168.0.46';
-var port = 8001;
+var port = 10001;
 
 server.listen(port, host, '50000', function(){
     console.log("web_server started");
 });
 
-console.log('hhi');

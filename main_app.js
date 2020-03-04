1361ad56174740b1af063631fc1f5ec7cae7ac9d
Leftddr <qhddnjs75@gmail.com>
2020-03-03 20:11:57 -0800

    login

--- main_app.js
+++ main_app.js
@@ -1,4 +1,7 @@
 var http = require('http');
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
 
 var server = http.createServer();
 
@@ -9,4 +12,18 @@ server.listen(port, host, '50000', function(){
     console.log("web_server started");
 });
 
-console.log('hhi');
+server.on('connection', function(socket){
+    var addr = socket.address();
+    console.log("client is logined : %s, %d", addr.address, addr.port);
+});
+
+server.on('request', function(req, res){
+    console.log("request is received");
+    var filename = './html/login.html';
+    var infile = fs.createReadStream(filename, {flags : 'r'});
+    infile.pipe(res);
+});
+
+server.on('close', function(){
+    console.log('connection closed');
+});

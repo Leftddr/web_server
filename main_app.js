7f63b4a03e03339f3ad3d237702f00f7211232aa
Leftddr <qhddnjs75@gmail.com>
2020-03-03 19:36:08 -0800

    i will expand my code

---
+++ main_app.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer()
+
+var host = '192.168.0.46'
+var port = 8001;
+
+server.listen(port, host, '50000', function(){
+    console.log("web_server started");
+});
